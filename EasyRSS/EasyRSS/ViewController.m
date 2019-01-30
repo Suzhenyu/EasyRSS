b027d3d26cd1e388eb284969910651fb4dbfa96e
Suzhenyu <983424749@qq.com>
2019-01-30 16:43:05 +0800

    [Add] 新引入第三方 RSS 解析器 MWFeedParser

--- EasyRSS/EasyRSS/ViewController.m
+++ EasyRSS/EasyRSS/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MWFeedParser/MWFeedParser.h>
 
-@interface ViewController ()
+@interface ViewController ()<MWFeedParserDelegate>
+
+@property (nonatomic, strong) MWFeedParser *feedParser;
 
 @end
 
@@ -16,7 +19,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL URLWithString:@"https://onevcat.com/feed.xml"];
+    _feedParser = [[MWFeedParser alloc] initWithFeedURL:url];
+    _feedParser.delegate = self;
+    if (![_feedParser parse]) {
+        NSLog(@"启动解析失败");
+    };
+}
+
+#pragma mark- MWFeedParserDelegate
+- (void)feedParserDidStart:(MWFeedParser *)parser {
+    NSLog(@"开始解析");
+}
+- (void)feedParser:(MWFeedParser *)parser didParseFeedInfo:(MWFeedInfo *)info {
+    NSLog(@"解析 FeedInfo 完毕，具体信息如下：%@", info);
+}
+- (void)feedParser:(MWFeedParser *)parser didParseFeedItem:(MWFeedItem *)item {
+    NSLog(@"解析 FeedItem 完毕，具体信息如下：%@", item);
+}
+- (void)feedParserDidFinish:(MWFeedParser *)parser {
+    NSLog(@"解析完毕");
+}
+- (void)feedParser:(MWFeedParser *)parser didFailWithError:(NSError *)error {
+    NSLog(@"解析出错，Error：%@", error);
 }
 
 
